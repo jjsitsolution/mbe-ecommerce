894fbe511298c30b68517aa029873aed999f2347
Paul Christian Rueda <nexuslifeline@gmail.com>
2018-01-13 17:53:12 +0800

    Added document type declaration and meta tag for the implementation of search engine optimization in the future.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
     <title>@yield('title')</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
